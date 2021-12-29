59470e7cb4d4944c692564f3dff8bd021ecc4182
Leo0114 <leonel0614@gmail.com>
2021-12-29 17:40:25 -0600

    first

---
+++ js/myjs.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+        
+
+
+  $(document).ready(function(){
+    var downn;
+    downn = {
+      coverTrigger: false,
+    };
+    $('select').formSelect(downn);});
+
+  
+    
+  $(document).ready(function(){
+    $('.datepicker').datepicker();
+  });
+
+
+  $(document).ready(function(){
+    $('.modal').modal();
+  });
+
